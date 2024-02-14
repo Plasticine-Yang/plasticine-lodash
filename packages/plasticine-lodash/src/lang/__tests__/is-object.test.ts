6336f25a5c3cf821e6092facd8b3e76ae6197481
yangwenfeng <yangwenfeng.0719@bytedance.com>
2024-02-14 12:34:35 +0800

    feat(plasticine-lodash): lang - isObject

---
+++ packages/plasticine-lodash/src/lang/__tests__/is-object.test.ts
@@ -0,0 +1,27 @@
+import { falsyValues, toArguments } from '@/testing-utils'
+
+import { isObject } from '../is-object'
+
+describe('lang - isObject', () => {
+  it('should return `true` for objects', () => {
+    expect(isObject(toArguments([1, 2, 3]))).toBe(true)
+    expect(isObject([1, 2, 3])).toBe(true)
+    expect(isObject(Object(false))).toBe(true)
+    expect(isObject(new Date())).toBe(true)
+    expect(isObject(new Error('oops!'))).toBe(true)
+    expect(isObject(Array.prototype.slice)).toBe(true)
+    expect(isObject({ a: 1 })).toBe(true)
+    expect(isObject(Object(0))).toBe(true)
+    expect(isObject(/x/)).toBe(true)
+    expect(isObject(Object('a'))).toBe(true)
+    expect(isObject(Object(Symbol('foo')))).toBe(true)
+  })
+
+  it('should return `false` for non-objects', () => {
+    const values = (falsyValues as any[]).concat(true, 1, 'foo', Symbol('foo'))
+    const expected = values.map(() => false)
+    const actual = values.map((value) => isObject(value))
+
+    expect(actual).toEqual(expected)
+  })
+})
---
+++ packages/plasticine-lodash/src/lang/is-object.ts
@@ -0,0 +1,5 @@
+export function isObject(value?: any): value is object {
+  const type = typeof value
+
+  return value !== null && (type === 'object' || type === 'function')
+}
