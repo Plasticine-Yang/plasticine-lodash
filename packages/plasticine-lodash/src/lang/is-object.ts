9d67f16398aeeba525d0929814d5694f99e15c9b
yangwenfeng <yangwenfeng.0719@bytedance.com>
2024-02-14 12:44:30 +0800

    chore(plasticine-lodash): add comments for lang - isObject

--- packages/plasticine-lodash/src/lang/is-object.ts
+++ packages/plasticine-lodash/src/lang/is-object.ts
@@ -1,3 +1,24 @@
+/**
+ * Checks if `value` is the
+ * [language type](http://www.ecma-international.org/ecma-262/7.0/#sec-ecmascript-language-types)
+ * of `Object`. (e.g. arrays, functions, objects, RegExps, `new Number(0)`, and `new String('')`)
+ *
+ * @param {*} value The value to check.
+ * @returns {boolean} Returns `true` if `value` is an object, else `false`.
+ * @example
+ *
+ * _.isObject({})
+ * // => true
+ *
+ * _.isObject([1, 2, 3])
+ * // => true
+ *
+ * _.isObject(_.noop)
+ * // => true
+ *
+ * _.isObject(null)
+ * // => false
+ */
 export function isObject(value?: any): value is object {
   const type = typeof value
 
